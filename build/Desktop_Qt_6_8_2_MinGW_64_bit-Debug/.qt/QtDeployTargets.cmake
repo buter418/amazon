4be62ac3ee92f2c3064d873274b2cf409719b3be
buter418 <62538783+buter418@users.noreply.github.com>
2025-03-12 08:02:52 -0700

    Delete build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug directory
    Trying to reset so can add again

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Super_Warehouse_Store_FILE C:/Users/Lukas Li/source/repos/Lukas Li projects/Super_Warehouse_Store/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/Super_Warehouse_Store.exe)
-set(__QT_DEPLOY_TARGET_Super_Warehouse_Store_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Super_Warehouse_Store_RUNTIME_DLLS C:/Users/Public/Downloads/QT/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Users/Public/Downloads/QT/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Users/Public/Downloads/QT/6.8.2/mingw_64/bin/Qt6Core.dll)
